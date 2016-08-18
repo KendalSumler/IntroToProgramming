536bcb3937fd23c32d172780bf57f0984092496d
KendalSumler <kendal.sumler@gmail.com>
2016-08-18 17:01:13 -0500

    Variable Exercise Homework

---
+++ enc_temp_folder/6da8cbd9d13cf7696b6265c1bf4bb/Source.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+/*int main()
+{	//Exercise 1
+	float a = 2 / 3 + 3 / 5;
+	int b = 2 % 3 + 3 % 5;
+	float c = 23 % 15 % -2;
+	int d = 25 * 1 / 2;
+	int e = 25 * 1.0 / 2;
+	int f = -23 * (1 / 2);
+	int g = 235 / 8 + 7;
+	int h = 235 / 8.0 + 7;
+	int i = ((20 + 1) / 2 - 2.0) / (23 + 3)*0.2;
+
+	//Example 1
+	printf("%i", a);
+	printf("\n");
+	//Example 2
+	printf("%i", b);
+	printf("\n");
+	//Example 3
+	printf("%i", c);
+	printf("\n");
+	//Example 4
+	printf("%i", d);
+	printf("\n");
+	//Example 5
+	printf("%i", e);
+	printf("\n");
+	//Example 6
+	printf("%i", f);
+	printf("\n");
+	//Example 7
+	printf("%i", g);
+	printf("\n");
+	//Example 8
+	printf("%i", h);
+	printf("\n");
+	//Example 9
+	printf("%i", i);
+	printf("\n");
+	system("pause");
+
+
+	return 0;
+}*/
+/* int main()
+{	//Exercise 2
+
+	//Example a
+	printf("Avg-Height \n");
+	
+	//Example b
+	printf("TaxR>$65000 \n");
+	
+	//Example c
+	printf("TaxR<$5000 \n");
+	
+	//Example d
+	printf("TotCoSteam \n");
+	
+	system("pause");
+
+	}*/
+/*int main() {
+	//Exercise 3
+	printf("a) Integer \n");
+	printf("b) Float \n");
+	printf("c) Character \n");
+	printf("d) Integer \n");
+	printf("e) Bool \n");
+	system("pause");
+}*/
+/*int main() {
+	//Exercise 4
+	int X = 6;
+	int Y = 9;
+	int tmp = Y;
+	Y = X;
+	X = tmp;
+	printf("%i \n", X);
+	printf("%i \n", Y);
+	system("pause");
+}*/
+int main() {
+	//Exercise 5
+	int i = 0;
+	float f = 0;
+	char ch = 'a';
+	std::cin >> i >> ch >> f;
+	std::cout << "i: " << i << "\nch: " << ch << "\nf: " << f << std::endl;	system("pause");	printf("i:1 \n");	printf("ch: A \n");	printf("f: 45");	system("pause");	printf("");	printf("");	printf("");	system("pause");	return 0;}
