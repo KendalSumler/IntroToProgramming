931d645d1fbefdf4d86ed4bc28546628bcddfb1e
KendalSumler <kendal.sumler@gmail.com>
2016-08-18 11:41:02 -0500

    Global Scopinmg and Variables
    wrote a program to define integers and do proper variables.

--- IntroToProgramming/Source.cpp
+++ IntroToProgramming/Source.cpp
@@ -7,3 +7,21 @@ int main()
 	system("pause");
 		return 0;
 }
+
+/*
+#include<iostream>
+int x = 25;
+int main()
+{
+
+int num = 5;
+{
+int x = 10;
+
+}
+printf("%i",num);
+system("pause");
+return 0;
+
+}
+*/
---
+++ Variables/Source.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int x = 25;
+int main()
+{
+
+	int num = 5;
+	{
+		int x = 10;
+
+	}
+	printf("%i", num);
+	system("pause");
+	return 0;
+
+}
