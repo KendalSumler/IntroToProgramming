74d471cf059a29434e6973e6734283949e92db1a
KendalSumler <kendal.sumler@gmail.com>
2016-08-24 17:52:09 -0500

    Extra corrections

--- Conditonal/Source.cpp
+++ Conditonal/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-int main() 
+/* //Exercise 1
+int main(){
 {
 	int y = 0;
 
@@ -14,29 +15,35 @@ int main()
 	std::cout << x;
 	system("pause");
 	return 0;
-}
+}*/
 	
+/*int main()
+{ // Exercise 2
+	int x = 757;
+	int y = 575;
+	
+	bool num = true;
+	if (x = true);
+	{
+		std::cout << x;
+	}
+	else if (y = true
+	{
+		std::cout << y;
+	}
+	system("pause");
+}*/
+/*int main()
+{
+
+
+}*/
 int main()
 {
-	int y = 575;
-	int x = 757;
-
-	}
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-	
+}
