8309a8d0a92db4bbaa81037fde9481c61cb63c89
KendalSumler <kendal.sumler@gmail.com>
2016-08-25 08:55:28 -0500

    Arrays
    Started doing course work for Arrays

---
+++ 50-60 Array Foolery/Source.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+using namespace std;
+ int main()
+/*{
+	 int array[11] = { 50,51,52,53,54,55,56,57,58,59,60 };
+
+
+	 for (int i = 0; i <= 10; i++)
+	 {
+		 std::cout << array[i] << "" << std::endl;
+	 }
+	 system("pause");
+	 return 0;
+}*/
+
+	//Exercise 1
+	//a.valid
+	//b.invalid "Too many initialized values"
+	//c.invalid "No whole numbers, can only contain whole numbers"
+	//d.invalid "Missing a value, also not enough value to complete the table"
+	//e.valid
+	//f.invalid "Nothing to initialize"
+	//g.invalid "Cannot contain a negative number of arrays"
+	//h.invalid "Syntax error, there are no scope operator"
+
+	//Exercise 2
+/*{
+	 int values[] = { 2, 6, 10, 14 };
+	 std::cout << values[4];
+	 //a.10
+	 //b.3
+	 //c.6
+	 //d.x is unidentified
+	 //e.-858993460
+	 
+	
+	 system("pause");
+	 return 0;
+}*/
+/*{
+	 int data[] = { 10,9,8,7,6,5,4,3,2,1 };
+	 for (int i = 0; i < 10; i++)
+	 {
+		 std::cout << data[i] << std::endl;
+	 }
+	 system("pause");
+	 return 0;
+}*/
+/*{
+	 int array[5];
+	 std::cin >> array[0];
+	 std::cin >> array[1];
+	 std::cin >> array[2];
+	 std::cin >> array[3];
+	 std::cin >> array[4];
+
+	 for (int i = 4; i >= 0; i--)
+	 {
+		 std::cout << array[i] << std::endl;
+	 }
+	 system("pause");
+	 return 0;
+}*/
+{
+	 int bull[10];
+	 int lrgnum= bull[0];
+	 int smallnum= bull[0];
+	 int ball=0;
+	 std::cin >> bull[0];
+	 std::cin >> bull[1];
+	 std::cin >> bull[2];
+	 std::cin >> bull[3];
+	 std::cin >> bull[4];
+	 std::cin >> bull[5];
+	 std::cin >> bull[6];
+	 std::cin >> bull[7];
+	 std::cin >> bull[8];
+	 std::cin >> bull[9];
+	 for (int ball = 0; ball < 9; ball++);
+
+	 if (lrgnum < bull[ball])
+		 lrgnum = bull[ball];
+	 if (smallnum > bull[ball])
+		 smallnum = bull[ball];
+		std::cout << lrgnum << smallnum << endl;
+		system("pause");
+		return 0;
+
+}
+
+
+
+
+
+
+
+
