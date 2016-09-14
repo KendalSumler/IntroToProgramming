ec54313527e2585fb5b99d29defcef8bbc9d14ae
KendalSumler <kendal.sumler@gmail.com>
2016-09-14 17:55:47 -0500

    String Assessment
    Work on my first assesement

---
+++ IntroToProgramming/MyString.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "MyString.h"
+using namespace std;
+char k[] = "Kenny";
+char j[] = "Jenny";
+char b[] = "Kneny";
+MyString::MyString() {}
+
+MyString::MyString(char w[])
+{
+	word = w;
+}
+int MyString::Size()
+{
+	int s = 0; //Length of the word
+	while k[] = 
+		i++
+		/*
+		Loop through each character in the string until we hit a terminating
+		incrementing the value of s each time a character is found.
+		*/
+		return s; //Returns the lenght of the word
+}
+bool MyString::StringCompare(MyString s)
+{
+	int i = 0;
+
+	/*
+	Loop through each character in both strings
+	as long as the characters at the index in both words or the same
+	increment to the next character. If the characters or different
+	retrun false(Breaks out the fucntion).
+	*/
+	return true;
+}
